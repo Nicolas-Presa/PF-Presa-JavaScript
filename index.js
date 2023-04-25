450d03cac92af497dc9576ac99fb68c31af1103b
Nicolas P <nicolaspresa26@gmail.com>
2023-04-24 22:02:40 -0300

    agregando funcion agregar y actualizar carrito

--- index.js
+++ index.js
@@ -48,23 +48,88 @@ const todosLosProductos = hamburguesas.concat(acompaniamientos, bebidas);
 
 
 const cuerpo = document.querySelector("#cuerpo");
-const heroContenedor = document.querySelector(".hero-contenedor");
-const heroBotonProducto = document.querySelector("#hero-boton-producto");
-const heroBotonTodos = document.querySelector("#hero-boton-todos");
-const heroTexto = document.querySelector("#hero-texto");
-const heroBotonHamburguesa = document.querySelector("#hero-boton-hamburguesa");
-const heroBotonAcompaniamiento = document.querySelector("#hero-boton-acompaniamiento");
-const heroBotonBebidas = document.querySelector("#hero-boton-bebidas");
-const heroBotonTodosLosProductos = document.querySelector("#hero-boton-todolosproductos");
+const verMiCarrito = document.querySelector(".ver-mi-carrito");
+const numerito = document.querySelector("#numerito");
 
+verMiCarrito.addEventListener("click", verCarritoDeCompras);
 
-function regresar() {
+const productosEnCarrito = [];
+
+function verCarritoDeCompras(){
     cuerpo.innerHTML = "";
-    cuerpo.classList.remove("cuerpo");
+    
+    const botonRegresar = document.createElement("button");
+    botonRegresar.classList.add("boton-regresar");
+    botonRegresar.innerText = "Regresar";
+
+    productosEnCarrito.forEach(producto => {
+        cuerpo.classList.add("hero-cuerpo5")
+        const div = document.createElement("div");
+        div.classList.add("hero-contenedor2");
+        div.innerHTML = `
+            <div class="producto">
+                <img class="producto__imagen" src= "${producto.imagen}" alt="${producto.nombre}">
+                <div class="producto__detalles">
+                    <h3 class="producto__titulo">${producto.nombre}</h3>
+                    <p class="producto__precio">$${producto.precio}</p>
+                    <button class="producto-eliminar" id="${producto.id}">Eliminar</button>
+                </div>
+            </div>
+        `
+        cuerpo.append(botonRegresar);
+        cuerpo.append(div);
+
+        botonRegresar.addEventListener("click", mostrarMain);
+    })
+}
+
+
+
+
+
+
+
+function actualizarBotonesAgregar(){
+    productoAgregar = document.querySelectorAll(".producto-agregar");
+
+    productoAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+function agregarAlCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = todosLosProductos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else{
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    acutalizarNumerito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function acutalizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
+
+
+
+
+function mostrarMain() {
+    cuerpo.innerHTML = "";
+    cuerpo.classList.remove("hero-cuerpo2");
+    cuerpo.classList.remove("hero-cuerpo3");
+    cuerpo.classList.remove("hero-cuerpo4");
     const div = document.createElement("div");
-    div.classList.add("hero-contenedor");
+    div.classList.add("cuerpo-hero");
     div.innerHTML = `
-        <div id="hero-texto" class="hero-texto">
+    <div id="hero-texto" class="hero-texto">
         <p>No te conformes con cantidad, busca calidad, y no te pierdas esta oportunidad</p>
     </div>
     <div id="hero-boton-producto" class="hero-boton-producto">
@@ -76,29 +141,37 @@ function regresar() {
         <button id="hero-boton-todolosproductos" class="hero-boton">Todos los productos</button>
     </div>
     `
-    const heroContenedor = document.querySelector(".hero-contenedor");
-    const heroBotonProducto = document.querySelector("#hero-boton-producto");
-    const heroBotonTodos = document.querySelector("#hero-boton-todos");
-    const heroTexto = document.querySelector("#hero-texto");
-    const heroBotonHamburguesa = document.querySelector("#hero-boton-hamburguesa");
-    const heroBotonAcompaniamiento = document.querySelector("#hero-boton-acompaniamiento");
-    const heroBotonBebidas = document.querySelector("#hero-boton-bebidas");
-    const heroBotonTodosLosProductos = document.querySelector("#hero-boton-todolosproductos");
-
     cuerpo.append(div);
-    
-    heroBotonHamburguesa.addEventListener("click", seleccionDeHamburguesas);
 
+const cuerpoHero = document.querySelector(".cuerpo-hero");
+const heroBotonProducto = document.querySelector("#hero-boton-producto");
+const heroBotonTodos = document.querySelector("#hero-boton-todos");
+const heroTexto = document.querySelector("#hero-texto");
+const heroBotonHamburguesa = document.querySelector("#hero-boton-hamburguesa");
+const heroBotonAcompaniamiento = document.querySelector("#hero-boton-acompaniamiento");
+const heroBotonBebidas = document.querySelector("#hero-boton-bebidas");
+const heroBotonTodosLosProductos = document.querySelector("#hero-boton-todolosproductos");
+
+heroBotonHamburguesa.addEventListener("click", () => seleccionDeHamburguesas());
+heroBotonAcompaniamiento.addEventListener("click", () => seleccionDeAcompaniamiento());
+heroBotonBebidas.addEventListener("click", () => seleccionDeBebidas());
+heroBotonTodosLosProductos.addEventListener("click", () => seleccionTodo());
 }
+mostrarMain();
+
+
+
+
 
 function seleccionDeHamburguesas() {
+    cuerpo.innerHTML = "";
+    
     const botonRegresar = document.createElement("button");
     botonRegresar.classList.add("boton-regresar");
     botonRegresar.innerText = "Regresar";
 
     hamburguesas.forEach(producto => {
-        cuerpo.classList.add("cuerpo");
-        heroContenedor.remove();
+        cuerpo.classList.add("hero-cuerpo2")
         const div = document.createElement("div");
         div.classList.add("hero-contenedor2");
         div.innerHTML = `
@@ -114,23 +187,21 @@ function seleccionDeHamburguesas() {
         cuerpo.append(botonRegresar);
         cuerpo.append(div);
 
-        botonRegresar.addEventListener("click", regresar);
+        botonRegresar.addEventListener("click", mostrarMain);
     })
+
+    actualizarBotonesAgregar();
 }
 
-
-heroBotonHamburguesa.addEventListener("click", seleccionDeHamburguesas);
-
-
-
 function seleccionDeAcompaniamiento() {
+    cuerpo.innerHTML = "";
+
     const botonRegresar = document.createElement("button");
     botonRegresar.classList.add("boton-regresar");
     botonRegresar.innerText = "Regresar";
 
     acompaniamientos.forEach(producto => {
-        cuerpo.classList.add("cuerpo");
-        heroContenedor.remove();
+        cuerpo.classList.add("hero-cuerpo2");
         const div = document.createElement("div");
         div.classList.add("hero-contenedor2");
         div.innerHTML = `
@@ -145,22 +216,22 @@ function seleccionDeAcompaniamiento() {
         `
         cuerpo.append(botonRegresar);
         cuerpo.append(div);
+
+        botonRegresar.addEventListener("click", mostrarMain);
     })
+
+    actualizarBotonesAgregar();
 }
 
-
-heroBotonAcompaniamiento.addEventListener("click", seleccionDeAcompaniamiento);
-
-
-
 function seleccionDeBebidas() {
+    cuerpo.innerHTML = "";
+
     const botonRegresar = document.createElement("button");
     botonRegresar.classList.add("boton-regresar");
     botonRegresar.innerText = "Regresar";
 
     bebidas.forEach(producto => {
-        cuerpo.classList.add("cuerpo2");
-        heroContenedor.remove();
+        cuerpo.classList.add("hero-cuerpo3");
         const div = document.createElement("div");
         div.classList.add("hero-contenedor2");
         div.innerHTML = `
@@ -175,21 +246,22 @@ function seleccionDeBebidas() {
         `
         cuerpo.append(botonRegresar);
         cuerpo.append(div);
+
+        botonRegresar.addEventListener("click", mostrarMain);
     })
+
+    actualizarBotonesAgregar();
 }
 
-
-heroBotonBebidas.addEventListener("click", seleccionDeBebidas);
-
-
 function seleccionTodo() {
+    cuerpo.innerHTML = "";
+
     const botonRegresar = document.createElement("button");
     botonRegresar.classList.add("boton-regresar");
     botonRegresar.innerText = "Regresar";
 
     todosLosProductos.forEach(producto => {
-        cuerpo.classList.add("cuerpo3");
-        heroContenedor.remove();
+        cuerpo.classList.add("hero-cuerpo4");
         const div = document.createElement("div");
         div.classList.add("hero-contenedor2");
         div.innerHTML = `
@@ -204,8 +276,10 @@ function seleccionTodo() {
         `
         cuerpo.append(botonRegresar);
         cuerpo.append(div);
+
+        botonRegresar.addEventListener("click", mostrarMain);
     })
+
+    actualizarBotonesAgregar();
 }
 
-
-heroBotonTodosLosProductos.addEventListener("click", seleccionTodo);
